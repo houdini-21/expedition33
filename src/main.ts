188545839fd670dfd33cda9e781834cdbe8d2af1
houdini <marinerocruzfer@gmail.com>
2025-10-14 13:07:48 -0600

    feat(security): enable helmet and restricted CORS for localhost
    Signed-off-by: houdini <marinerocruzfer@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalValidationPipe } from './common/pipes/validation.pipe';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(GlobalValidationPipe);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(helmet());
+  app.enableCors();
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 4000);
 }
 void bootstrap();
